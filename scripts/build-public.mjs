d5daa93441118502553ade0a6b8bdbca5a0c984d
GarrettPetersen <garrett.m.petersen@gmail.com>
2026-04-13 22:51:29 -0700

    add favicon

--- scripts/build-public.mjs
+++ scripts/build-public.mjs
@@ -16,6 +16,9 @@ await cp(path.join(root, 'data'), path.join(out, 'data'), { recursive: true });
 if (existsSync(path.join(root, 'og-image.png'))) {
   await cp(path.join(root, 'og-image.png'), path.join(out, 'og-image.png'));
 }
+if (existsSync(path.join(root, 'favicon.ico'))) {
+  await cp(path.join(root, 'favicon.ico'), path.join(out, 'favicon.ico'));
+}
 if (existsSync(path.join(root, 'sounds'))) {
   await cp(path.join(root, 'sounds'), path.join(out, 'sounds'), { recursive: true });
 }
